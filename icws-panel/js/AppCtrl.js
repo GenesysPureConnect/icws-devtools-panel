10da1ad6c3df20813aa5fad8a7453af561f75f19
Daniel White <daniel.white@inin.com>
2017-02-15 14:28:23 -0500

    adding request/response handling

--- icws-panel/js/AppCtrl.js
+++ icws-panel/js/AppCtrl.js
@@ -3,6 +3,25 @@ angular.module('IcwsPanel', []).controller('AppCtrl', ['$scope', '$window', func
 
     this.messages = [];
 
+    this.requests = {};
+
+    function handleRequest(message) {
+        var content = message.content;
+        ctrl.requests[content.correlationId] = content;
+        content.requestTimestamp = message.timestamp;
+        content.state = 'pending';
+    }
+
+    function handleResponse(message) {
+        var request = ctrl.requests[message.content.correlationId];
+        if (request) {
+            request.state = message.content.callback;
+            request.status = message.content.status;
+            request.responseTimestamp = message.timestamp;
+            request.responseContent = message.content.content;
+        }
+    }
+
     // Create a connection to the background page
     var backgroundPageConnection = chrome.runtime.connect({
         name: 'icws-panel'
@@ -18,6 +37,10 @@ angular.module('IcwsPanel', []).controller('AppCtrl', ['$scope', '$window', func
             console.log(`Status message from background page: ${message.data}`);
         } else if (message.type === 'icws-message') {
             $scope.$apply(() => this.messages.push(message));
+        } else if (message.type === 'icws-request') {
+            $scope.$apply(() => handleRequest(message));
+        } else if (message.type === 'icws-response') {
+            $scope.$apply(() => handleResponse(message));
         }
     });
 }]);
