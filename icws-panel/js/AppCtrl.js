21fce8bd3262d4c97b841941bc933c65187884c7
priceld <priceld5@gmail.com>
2017-02-16 09:38:01 -0500

    collecting some basic session info data

--- icws-panel/js/AppCtrl.js
+++ icws-panel/js/AppCtrl.js
@@ -13,11 +13,40 @@ angular.module('IcwsPanel', []).controller('AppCtrl', ['$scope', '$window', func
         tabId: chrome.devtools.inspectedWindow.tabId
     });
 
+    this.sessionData = {
+        apiCallCount: 0,
+        subscriptionCount: 0
+    };
+
     backgroundPageConnection.onMessage.addListener(message => {
         if (message.type === 'status') {
             console.log(`Status message from background page: ${message.data}`);
         } else if (message.type === 'icws-message') {
             $scope.$apply(() => this.messages.push(message));
+        } else if (message.type === 'icws-request') {
+            this.sessionData.apiCallCount++;
+            console.log(message.content);
+            if (!this.sessionData.sessionId) {
+                var matches = /\/api\/(\w+)\/icws\/(\d+)/.exec(message.content.url);
+                if (matches) {
+                    this.sessionData.sessionId = matches[1];
+                    this.sessionData.icServerName = matches[2];
+                }
+            }
+
+            if (/\/icws\/[^\/]+\/messaging\/subscriptions\//.test(message.content.url)) {
+                // TODO: right now this only tracks subscriptions since the panel has been open
+                if (message.content.verb === "DELETE") {
+                    this.sessionData.subscriptionCallCount--;
+                } else {
+                    this.sessionData.subscriptionCallCount++;
+                }
+            }
+            $scope.$digest();
+        } else if (message.type === 'icws-response') {
+
         }
     });
+
+
 }]);
