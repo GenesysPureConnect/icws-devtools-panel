57e4a6b81042e44d06a837978d0391e507c90bbe
priceld <priceld5@gmail.com>
2017-02-16 10:52:31 -0500

    hook into chrome api to get request sizes, update ui, use ES6!

--- icws-panel/js/AppCtrl.js
+++ icws-panel/js/AppCtrl.js
@@ -18,7 +18,11 @@ angular.module('IcwsPanel', []).controller('AppCtrl', ['$scope', '$window', func
         subscriptionCount: 0,
         responseSize: {
             headers: 0,
-            content: 0
+            body: 0
+        },
+        requestSize: {
+            headers: 0,
+            body: 0
         }
     };
 
@@ -27,14 +31,15 @@ angular.module('IcwsPanel', []).controller('AppCtrl', ['$scope', '$window', func
             console.log(`Status message from background page: ${message.data}`);
         } else if (message.type === 'icws-message') {
             $scope.$apply(() => this.messages.push(message));
+            // TODO: get over-the-wire size?
         } else if (message.type === 'icws-request') {
             this.sessionData.apiCallCount++;
             // console.log(message.content);
             if (!this.sessionData.sessionId) {
-                var matches = /\/api\/(\w+)\/icws\/(\d+)/.exec(message.content.url);
+                let matches = /\/api\/(\w+)\/icws\/(\d+)/.exec(message.content.url);
                 if (matches) {
-                    this.sessionData.sessionId = matches[1];
-                    this.sessionData.icServerName = matches[2];
+                    this.sessionData.icServerName = matches[1];
+                    this.sessionData.sessionId = matches[2];
                 }
             }
 
@@ -49,8 +54,18 @@ angular.module('IcwsPanel', []).controller('AppCtrl', ['$scope', '$window', func
             $scope.$digest();
         } else if (message.type === 'icws-response') {
             this.sessionData.responseSize.headers += message.content.size.headers;
-            this.sessionData.responseSize.content += message.content.size.content;
+            this.sessionData.responseSize.body += message.content.size.body;
             $scope.$digest();
+        }
+    });
+
+    // We can't get the request size from ICWS itself (a limitation of XmlHttpRequest)
+    // So use the chrome devtools network API
+    chrome.devtools.network.onRequestFinished.addListener(data => {
+        const request = data.request;
+        if (/\/icws\//.test(request.url)) {
+            this.sessionData.requestSize.headers += request.headersSize;
+            this.sessionData.requestSize.body += request.bodySize;
         }
     });
 }]);
