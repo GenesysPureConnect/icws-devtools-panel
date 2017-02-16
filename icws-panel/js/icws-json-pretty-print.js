7c93b75a1933bf6ddf085e0fa1caef96dcabcfe1
Jason Mobley <github@jasonmobley.email>
2017-02-16 11:47:43 -0500

    Update pretty print to tag quotes and make them invisible

--- icws-panel/js/icws-json-pretty-print.js
+++ icws-panel/js/icws-json-pretty-print.js
@@ -2,6 +2,7 @@
     var directiveName = 'icwsJsonPrettyPrint';
 
     function jsonToHtml(input) {
+        if (!input) { return ''; }
         let json = (typeof input === 'string') ? input : JSON.stringify(input, undefined, 2);
         json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
         return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
@@ -17,9 +18,9 @@
             } else if (/null/.test(match)) {
                 cls = 'null';
             }
-            // Strip quotes from key names
+            // Replace quotes in key names with <q> tags
             if (cls === 'key') {
-                match = match.replace('"', '');
+                match = match.replace(/^"([^"]+)":$/, '<span class="quote">&quot;</span>$1<span class="quote">&quot;</span>:');
             }
             return '<span class="' + cls + '">' + match + '</span>';
         });
